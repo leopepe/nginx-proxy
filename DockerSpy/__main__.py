81dead7e95b905bfcab788f2d14e64f76b8dac64
Leonardo Pepe <lpepefreitas@gmail.com>
2015-07-07 15:46:28 -0300

    removing DockerSpy script from image

--- DockerSpy/__main__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'leonardo'
-
-from . import DockerSpy
-from json import dumps
-
-if __name__ == '__main__':
-    docker = DockerSpy()
-    container_env = docker.env
-    print(dumps(container_env, indent=4))
